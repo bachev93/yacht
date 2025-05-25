c47733083789a98c738d3c04be33053d59da6de0
Sergey Bachev <bachev93@mail.ru>
2025-05-26 00:28:27 +0300

    Add servo to yacht sketch

--- yacht/yacht.ino
+++ yacht/yacht.ino
@@ -1,7 +1,12 @@
+#include <Servo.h>
+
 #define pinX A2    // ось X джойстика
 #define pinY A1    // ось Y джойстика
 #define swPin 2    // кнопка джойстика
 #define ledPin 13  // светодиод на Pin 13
+#define SERVO_PIN 11
+
+Servo servo;
 
 void setup() {
   Serial.begin(9600);
@@ -12,6 +17,8 @@ void setup() {
 
   pinMode(swPin, INPUT);
   digitalWrite(swPin, HIGH);
+
+  servo.attach(SERVO_PIN);
 }
 
 void loop() {
@@ -24,4 +31,13 @@ void loop() {
   Serial.print(X);     // выводим в Serial Monitor
   Serial.print("\t");  // табуляция
   Serial.println(Y);
+
+  servo.write(0);
+  delay(1000);
+
+  servo.write(90);
+  delay(1000);
+
+  servo.write(180);
+  delay(1000);
 }
